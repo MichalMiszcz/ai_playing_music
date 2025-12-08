5e4679083d9cbd633fd673e9c9b9952f2f60e155
Michał Krawczyk <michal.krawczyk.rogi@gmail.com>
2025-12-09 00:30:22 +0100

    New midi files

--- src/new_data_generator/generating_note_sheets.py
+++ src/new_data_generator/generating_note_sheets.py
@@ -175,14 +175,14 @@ def midi2jpg(midi_folder_path, image_folder_path, mode='midi', instances_count =
 
 if __name__ == "__main__":
     # midi_raw_folder_path = "generated_songs_raw"
-    midi_raw_folder_path = "../src/all_data/generated/generated_complex_midi_test/my_midi_files"
-    xml_raw_folder_path = "../src/all_data/generated/generated_complex_midi_test/my_xml_files"
+    midi_raw_folder_path = "../src/all_data/generated/generated_complex_midi_raw/my_midi_files"
+    xml_raw_folder_path = "../src/all_data/generated/generated_complex_midi_raw/my_xml_files"
     # processed_folder_path = "data/processed_midi"
     # processed_folder_path = "generated_songs_processed"
     processed_folder_path = "../src/all_data/generated/generated_complex_midi_processed/my_midi_files"
     # image_folder_path = "data/images"
     # image_folder_path = "my_images/my_midi_images"
-    image_folder_path = "../src/all_data/generated/my_complex_images_test/my_midi_images/my_midi_files"
+    image_folder_path = "../src/all_data/generated/my_complex_images/my_midi_images/my_midi_files"
 
     # process_midi(midi_raw_folder_path, processed_folder_path, max_duration=24.0, fixed_bpm=120, add_track_name=True)
     # midi2jpg(processed_folder_path, image_folder_path)
