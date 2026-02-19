2b0b356773dd32023f60a3d7c457c122a131da41
Michał Krawczyk <michal.krawczyk.rogi@gmail.com>
2026-02-19 14:53:19 +0100

    Optimisation for lstm tested v.2

--- src/test/lstm_sequence_to_midi.py
+++ src/test/lstm_sequence_to_midi.py
@@ -6,11 +6,11 @@ random_times = random.choices(range(1000, 10000), k=10)
 lstm_sequence = [[n, dt] for n in random_notes for dt in random_times]
 print(lstm_sequence)
 
-changed_lstm_sequence = []
-for note, delta_time in lstm_sequence:
-    velocity = 0
-    changed_lstm_sequence.append([note, velocity, delta_time])
-    velocity = 90
-    changed_lstm_sequence.append([note, velocity, delta_time])
+changed_lstm_sequence = [
+    [note, velocity, delta_time]
+    for note, delta_time in lstm_sequence
+    for velocity in (90, 0)
+]
+
 
 print(changed_lstm_sequence)
