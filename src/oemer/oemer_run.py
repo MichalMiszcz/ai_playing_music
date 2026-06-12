6a95315d64a522d3094056a10dbcb1553144a863
MichalMiszcz <michal.krawczyk.rogi@gmail.com>
2026-06-12 13:05:07 +0200

    Generated data by oemer

--- src/oemer/oemer_run.py
+++ src/oemer/oemer_run.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-import onnxruntime as ort
+from music21 import converter, tempo, instrument
 
 import time
 import re
@@ -31,12 +31,13 @@ def run_oemer(image_path, output_folder):
 if __name__ == "__main__":
     image_folder_path = "src/all_data/data_to_analyze/low_res"
     output_folder_path = "src/all_data/model_generated/oemer"
-    previous_number = 42
-    skipped_numbers = [21, 22, 32, 33, 38, 40]
+    previous_number = 0
+    max_num = 100
+    skipped_numbers = []
 
     for folder in os.listdir(image_folder_path):
         number = int(re.findall(r'\d+', folder)[0])
-        if (previous_number < number < 100) or (number in skipped_numbers):
+        if (previous_number < number <= max_num) or (number in skipped_numbers):
             print(number)
             subfolder_path = os.path.join(image_folder_path, folder)
             for file in os.listdir(subfolder_path):
@@ -44,11 +45,26 @@ if __name__ == "__main__":
                 output_path = os.path.join(output_folder_path, folder)
 
                 start = time.time()
-                run_oemer(image_path, output_path)
+                # run_oemer(image_path, output_path)
                 end = time.time()
-
                 time_taken = end - start
                 print(f"Time taken: {time_taken}s\n")
+
+                output_midi_folder = os.path.join(output_folder_path, 'midi')
+                if not os.path.exists(output_midi_folder):
+                    os.makedirs(output_midi_folder)
+
+                mxml_name = folder + "-1.musicxml"
+                input_mxml = os.path.join(output_path, mxml_name)
+                score = converter.parse(input_mxml)
+                score.insert(0, tempo.MetronomeMark(number=120))
+
+                for part in score.parts:
+                    part.insert(0, instrument.Piano())
+
+                midi_name = folder + ".midi"
+                output_midi = os.path.join(output_midi_folder, midi_name)
+                score.write("midi", output_midi)
 
 
 
